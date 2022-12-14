edb0fcbb890c04192afc55c1004f95601cff1599
Max Power <alex@alexaa.dev>
2022-12-13 23:55:03 -0300

    feat(apollo): server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello, world")
+import { ApolloServer } from "apollo-server";
+import { schema } from "./schema";
+import { createContext } from "./context";
+
+const app = new ApolloServer({
+  schema,
+  context: createContext,
+});
+
+app
+  .listen({
+    port: 8080,
+  })
+  .then(({ url }) => console.log(`Server is running on ${url}`));
