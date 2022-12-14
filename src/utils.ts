081edd5312d27d5a968c8501b1d70d5760a8999c
Max Power <alex@alexaa.dev>
2022-12-14 15:45:53 -0300

    fix(apollo): no need to recheck

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,17 @@
 import { verify } from "jsonwebtoken";
-import {Context} from "./context";
+import { Context } from "./context";
 
 type Token = {
-	userId: string;
-}
+  userId: string;
+};
 
 export function getUserId(context: Context) {
-	const authHeader = context.req.get("Authorization");
-	const secret = process.env.APP_SECRET;
+  const authHeader = context.req.get("Authorization");
+  const secret = process.env.APP_SECRET;
 
-	if (!secret) throw new Error("$APP_SECRET is not defined");
-
-	if (authHeader) {
-		const token = authHeader.replace("Bearer ", "");
-		const verifiedToken = verify(token, secret) as Token;
-		return verifiedToken && verifiedToken.userId;
-	}
+  if (authHeader) {
+    const token = authHeader.replace("Bearer ", "");
+    const verifiedToken = verify(token, secret) as Token;
+    return verifiedToken && verifiedToken.userId;
+  }
 }
